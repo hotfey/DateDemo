db6557d108ec9119a4e0f3aeb2dfea52f7ad24fa
hotfey <hotfey@foxmail.com>
2015-12-30 19:06:25 +0800

    optimized

---
+++ DateDemo/src/test/java/com/hotfey/date/DateAssignTest.java
@@ -0,0 +1,19 @@
+package com.hotfey.date;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DateAssignTest {
+	@Ignore
+	@Test
+	public void assignDayTest() {
+		System.out.println(DateAssign.assignDay(0));
+	}
+
+	@Ignore
+	@Test
+	public void assignHourTest() {
+		System.out.println(DateAssign.assignHour(0));
+	}
+
+}
